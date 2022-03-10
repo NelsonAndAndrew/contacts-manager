1c311ba78ed076a558e35c923dae70ac158e8485
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-10 14:19:50 -0600

    created psvm in app

---
+++ src/ContactsApp.java
@@ -0,0 +1,18 @@
+package PACKAGE_NAME;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsApp {
+
+
+    public static void main(String[] args) {
+
+//     CODE GOES HERE LOL
+
+
+//        END OF MAIN
+    }
+
+//END OF CLASS
+}
