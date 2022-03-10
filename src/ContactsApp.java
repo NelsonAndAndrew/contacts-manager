e2e93a353ff490aefd97bda15f6b4047cf8f2a8a
Andrew Lange <andrew.lange13@gmail.com>
2022-03-10 15:28:06 -0600

    adds new contact adder

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -46,6 +48,8 @@ public class ContactsApp {
             String newNumber = scan.next();
             System.out.print("Enter email address:  ");
             String newEmail = scan.next();
+            List<String> newContact = Arrays.asList(newName, newNumber, newEmail);
+            System.out.println(newContact);
 
         }
 
