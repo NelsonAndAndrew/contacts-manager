4671ed82df48d6cdecb6b8eb853a7084c0effcb1
Andrew Lange <andrew.lange13@gmail.com>
2022-03-10 16:23:07 -0600

    adds exit function and doWhile

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -26,57 +26,61 @@ public class ContactsApp {
         System.out.println("dataFile = " + dataFile);
 
         List<String> contactsList = new ArrayList<>();
+        do {
+            System.out.println("1. View contacts.");
+            System.out.println("2. Add a new contact.");
+            System.out.println("3. Search a contact by name.");
+            System.out.println("4. Delete an existing contact.");
+            System.out.println("5. Exit.");
+            System.out.print("Enter an option (1, 2, 3, 4 or 5):  ");
+            int userInput = scan.nextInt();
 
-        System.out.println("1. View contacts.");
-        System.out.println("2. Add a new contact.");
-        System.out.println("3. Search a contact by name.");
-        System.out.println("4. Delete an existing contact.");
-        System.out.println("5. Exit.");
-        System.out.print("Enter an option (1, 2, 3, 4 or 5):  ");
-        int userInput = scan.nextInt();
 
-        //Conditional Start
-        if (userInput == 1) {
-            System.out.println(Files.readAllLines(dataFile));
-        } else if (userInput == 2) {
-            System.out.print("Enter a name :  ");
-            String newName = scan.next();
-            System.out.print("Enter phone number:  ");
-            String newNumber = scan.next();
-            System.out.print("Enter email address:  ");
-            String newEmail = scan.next();
-            List<String> newContact = Arrays.asList(newName, newNumber, newEmail);
-            Files.write(dataFile, newContact, StandardOpenOption.APPEND);
+            //Conditional Start
+            if (userInput == 1) {
+                System.out.println(Files.readAllLines(dataFile));
+            } else if (userInput == 2) {
+                System.out.print("Enter a name :  ");
+                String newName = scan.next();
+                System.out.print("Enter phone number:  ");
+                String newNumber = scan.next();
+                System.out.print("Enter email address:  ");
+                String newEmail = scan.next();
+                List<String> newContact = Arrays.asList(newName, newNumber, newEmail);
+                Files.write(dataFile, newContact, StandardOpenOption.APPEND);
 //            System.out.println(Files.readAllLines(dataFile));
-        } else if (userInput == 3) {
-            System.out.print("Enter a name to search:  ");
-            String userSearch = scan.next();
-            contactsList = Files.readAllLines(dataFile);
-            System.out.println(contactsList);
-            for (int i = 0; i < contactsList.size(); i++) {
-                if (contactsList.get(i).equals(userSearch)) {
-                    System.out.println(contactsList.get(contactsList.indexOf(userSearch)));
-                    System.out.println(contactsList.get(contactsList.indexOf(userSearch) + 1));
-                    System.out.println(contactsList.get(contactsList.indexOf(userSearch) + 2));
-                }
+            } else if (userInput == 3) {
+                System.out.print("Enter a name to search:  ");
+                String userSearch = scan.next();
+                contactsList = Files.readAllLines(dataFile);
+//                System.out.println(contactsList);
+                for (int i = 0; i < contactsList.size(); i++) {
+                    if (contactsList.get(i).equals(userSearch)) {
+                        System.out.println(contactsList.get(contactsList.indexOf(userSearch)));
+                        System.out.println(contactsList.get(contactsList.indexOf(userSearch) + 1));
+                        System.out.println(contactsList.get(contactsList.indexOf(userSearch) + 2));
+                    }
 
-            }
-        } else if (userInput == 4) {
-            System.out.print("Who you want to get rid of:  ");
-            String userDelete = scan.next();
-            contactsList = Files.readAllLines(dataFile);
-            List<String> deleteList = new ArrayList<>();
-            for (int i = 0; i < contactsList.size(); i++) {
-                if (contactsList.get(i).equals(userDelete)) {
-                    i+=2;
-                    continue;
                 }
-                deleteList.add(contactsList.get(i));
+            } else if (userInput == 4) {
+                System.out.print("Who you want to get rid of:  ");
+                String userDelete = scan.next();
+                contactsList = Files.readAllLines(dataFile);
+                List<String> deleteList = new ArrayList<>();
+                for (int i = 0; i < contactsList.size(); i++) {
+                    if (contactsList.get(i).equals(userDelete)) {
+                        i += 2;
+                        continue;
+                    }
+                    deleteList.add(contactsList.get(i));
 //                contactsList = deleteList;
+                }
+                Files.write(dataFile, deleteList);
+//                System.out.println(deleteList);
+            } else if (userInput == 5) {
+                System.exit(0);
             }
-            Files.write(dataFile, deleteList);
-            System.out.println(deleteList);
-        }
+        } while (true);
 
 
 //        END OF MAIN
