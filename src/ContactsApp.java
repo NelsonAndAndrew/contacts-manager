153396799a7df54b50180ef8f7295ef7c2511a35
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-10 14:38:01 -0600

    added paths

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -1,11 +1,21 @@
 package contactsManagerApp;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ContactsApp {
 
 
     public static void main(String[] args) {
 
 //     CODE GOES HERE LOL
+
+        String directory = "./src";
+        String filename = "contacts.txt";
+
+        Path dataFile = Paths.get(directory, filename);
+
+        System.out.println("dataFile = " + dataFile);
 
 
 //        END OF MAIN
